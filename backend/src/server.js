9d7b094f228af456658b834f9e12ba2f8feb01af
davis-1994 <davis.jankovics1@gmail.com>
2026-02-22 17:35:42 +0200

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -7,11 +8,26 @@ import messageRoutes from './routes/message.route.js';
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+// make ready for deployment
+if (process.env.NODE_ENV === 'production') {
+  // Path to frontend/dist
+  const distPath = path.join(__dirname, '../frontend', 'dist');
+
+  // Serve static assets
+  app.use(express.static(distPath));
+
+  // SPA fallback (must be AFTER static)
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
